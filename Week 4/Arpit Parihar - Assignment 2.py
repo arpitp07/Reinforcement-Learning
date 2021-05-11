975a625cc1341a4eb15a300a6527ac57c78c8cc7
arpitp07 <arpitparihar07@uchicago.edu>
2021-05-11 16:30:42 -0500

    Plotted optimal policy for policy iteration

--- Week 4/Arpit Parihar - Assignment 2.py
+++ Week 4/Arpit Parihar - Assignment 2.py
@@ -1,25 +1,26 @@
 # %% [markdown]
-# # Reinforcement Learning - Assignment 2
-# ## Arpit Parihar
-# ## 05/04/2021
-# ****
+#  # Reinforcement Learning - Assignment 2
+#  ## Arpit Parihar
+#  ## 05/04/2021
+#  ****
 # %% [markdown]
-# A child has available a certain number of ice cream scoops every day, $s$. The child can store a number of scoops for the next day $a$ and eat the remainder scoops $c = s − a$. Mathematically,
-# $$s = a + c$$
-# where $a \in \{0, 1, .., s\}$. The number of scoops available the next day, $s'$, is equal to the number of scoops stored over night, $a$, plus an additional scoops provided by the parents, $e$. The number of scoops available the next day is given by:
-#
-# $$
-# \begin{eqnarray}
-# s'= && a + e' \\
-#   = && (s - c) + e'
-# \end{eqnarray}
-# $$
-#
-# where $e' \in \{0, 1, 2\}$. $e'$ is known after action $a$ is taken. The child can store up to $2$ scoops in the fridge every day which implies that $a \in \{0, 1, 2\}$ and $s \in \{0, 1, 2, 3, 4\}$. The transition probability matrix from $e$ to $e'$ is given by:
-#
-# $$P = \begin{bmatrix} 0.8 & 0.1 & 0.1 \\ 0.01 & 0.98 & 0.01 \\ 0.1 & 0.1 & 0.8 \end{bmatrix}$$
+#  A child has available a certain number of ice cream scoops every day, $s$. The child can store a number of scoops for the next day $a$ and eat the remainder scoops $c = s − a$. Mathematically,
+#  $$s = a + c$$
+#  where $a \in \{0, 1, .., s\}$. The number of scoops available the next day, $s'$, is equal to the number of scoops stored over night, $a$, plus an additional scoops provided by the parents, $e$. The number of scoops available the next day is given by:
+# 
+#  $$
+#  \begin{eqnarray}
+#  s'= && a + e' \\
+#    = && (s - c) + e'
+#  \end{eqnarray}
+#  $$
+# 
+#  where $e' \in \{0, 1, 2\}$. $e'$ is known after action $a$ is taken. The child can store up to $2$ scoops in the fridge every day which implies that $a \in \{0, 1, 2\}$ and $s \in \{0, 1, 2, 3, 4\}$. The transition probability matrix from $e$ to $e'$ is given by:
+# 
+#  $$P = \begin{bmatrix} 0.8 & 0.1 & 0.1 \\ 0.01 & 0.98 & 0.01 \\ 0.1 & 0.1 & 0.8 \end{bmatrix}$$
 # %% [markdown]
-# Importing modules
+#  Importing modules
+
 # %%
 import numpy as np
 import pandas as pd
@@ -33,8 +34,10 @@ import random
 from matplotlib.ticker import FormatStrFormatter
 import warnings
 warnings.filterwarnings('ignore')
+
 # %% [markdown]
-# - **Construct the transition probability from $(s,e)$ to $(s′,e′)$ for each action $a \in \{0,1,2\}$.**
+#  - **Construct the transition probability from $(s,e)$ to $(s′,e′)$ for each action $a \in \{0,1,2\}$.**
+
 # %%
 s = range(5)
 e = range(3)
@@ -62,16 +65,20 @@ for x in range(len(P_mat)):
     print(f'Transition matrix for a = {x}:\n')
     P_mat[x]
     print('\n')
+
 # %% [markdown]
-# - **The child subjected utility from eating ice cream is $log(c+1)$ if $c≥0$ and $−\infty$ otherwise. Construct the reward matrix for each transition $(s,e,a)$ and $(s′,e′)$.**
+#  - **The child subjected utility from eating ice cream is $log(c+1)$ if $c≥0$ and $−\infty$ otherwise. Construct the reward matrix for each transition $(s,e,a)$ and $(s′,e′)$.**
+
 # %%
 R_mat = [pd.DataFrame(R[x, :, :], columns=states, index=states) for x in a]
 for x in range(len(R_mat)):
     print(f'Reward matrix for a = {x}:\n')
     R_mat[x]
     print('\n')
+
 # %% [markdown]
-# - **Apply the value iteration approach to compute the value function for each state $(s,e)$ and optimal policy. Iterate $600$ times.**
+#  - **Apply the value iteration approach to compute the value function for each state $(s,e)$ and optimal policy. Iterate $600$ times.**
+
 # %%
 # Creating q vector
 q = np.zeros((len(a), len(s) * len(e)), dtype=np.float64)
@@ -95,8 +102,10 @@ for t in range(1, T + 1):
 
 v = pd.DataFrame(v.T, columns=states)
 d = pd.DataFrame(d.T, columns=states)
+
 # %% [markdown]
-# - **For each $e$, plot the value function $v(s,e)$ with $s$ on the x-axis. (hint: there should be 3 lines plots, one for each $e\in\{0,1,2\}$).**
+#  - **For each $e$, plot the value function $v(s,e)$ with $s$ on the x-axis. (hint: there should be 3 lines plots, one for each $e\in\{0,1,2\}$).**
+
 # %%
 plot_data = np.zeros((len(s), len(e)))
 for i in range(len(states)):
@@ -111,8 +120,10 @@ plt.ylabel('Expected Value')
 plt.legend(e)
 plt.title('Average Value for each s and e')
 plt.show();
+
 # %% [markdown]
-# **For each $e$, plot the optimal policy for storing ice cream scoops $a(s,e)$ with $s$ on the x-axis. (hint: there should be 3 lines plots, one for each $e\in\{0,1,2\}$).**
+#  **For each $e$, plot the optimal policy for storing ice cream scoops $a(s,e)$ with $s$ on the x-axis. (hint: there should be 3 lines plots, one for each $e\in\{0,1,2\}$).**
+
 # %%
 plot_data = np.zeros((len(s), len(e)))
 for i in range(len(states)):
@@ -128,8 +139,10 @@ plt.yticks(a)
 plt.legend(e)
 plt.title('Scoops Available vs Scoops Saved')
 plt.show();
+
 # %% [markdown]
-# - **For each $e$, plot the optimal policy for consuming ice cream scoops $c(s,e)$ with $s$ on the x-axis. (hint: there should be 3 lines plots, one for each $e\in\{0,1,2\}$).**
+#  - **For each $e$, plot the optimal policy for consuming ice cream scoops $c(s,e)$ with $s$ on the x-axis. (hint: there should be 3 lines plots, one for each $e\in\{0,1,2\}$).**
+
 # %%
 plot_data = np.zeros((len(s), len(e)))
 for i in range(len(states)):
@@ -145,11 +158,15 @@ plt.yticks(a)
 plt.legend(e)
 plt.title('Scoops Available vs Scoops Eaten')
 plt.show();
+
+
 # %%
 P_optim = np.array([P[j, i, :] for (i, j) in enumerate(d.iloc[-1].astype('int'))])
 R_optim = np.array([R[j, i, :] for (i, j) in enumerate(d.iloc[-1].astype('int'))])
+
 # %% [markdown]
-# - **Simulate a sequence of $e$ and set an initial value for $s$. Given the optimal policy, calculate and plot the evolution of $a, c,$ and $s$ over time.**
+#  - **Simulate a sequence of $e$ and set an initial value for $s$. Given the optimal policy, calculate and plot the evolution of $a, c,$ and $s$ over time.**
+
 # %%
 p_e_dat = pd.DataFrame(p_e)
 T = 600
@@ -171,6 +188,8 @@ for i in range(T):
     
     plot_data.iloc[i, :] = [state, a_sim, c_sim, state_new, s_sim]
     state = state_new
+
+
 # %%
 plt.plot(plot_data['a'], drawstyle='steps')
 plt.yticks(a)
@@ -178,6 +197,8 @@ plt.xlabel('Step')
 plt.ylabel('Scoops Saved, a')
 plt.title('Scoops Saved per Iteration')
 plt.show();
+
+
 # %%
 plt.plot(plot_data['c'], drawstyle='steps')
 plt.yticks(a)
@@ -185,6 +206,8 @@ plt.xlabel('Step')
 plt.ylabel('Scoops Eaten, c')
 plt.title('Scoops Eaten per Iteration')
 plt.show();
+
+
 # %%
 plt.plot(plot_data['s'], drawstyle='steps')
 plt.yticks(s)
@@ -192,8 +215,10 @@ plt.xlabel('Step')
 plt.ylabel('Scoops Available, s')
 plt.title('Scoops Available per Iteration')
 plt.show();
+
 # %% [markdown]
-# - **Construct the transition probability and reward matrices between $(s,e)$ and $(s′,e′)$ that produces the highest expected discount rewards. (hint: you need to use the optimal policy)**
+#  - **Construct the transition probability and reward matrices between $(s,e)$ and $(s′,e′)$ that produces the highest expected discount rewards. (hint: you need to use the optimal policy)**
+
 # %%
 P_optim = pd.DataFrame(
     np.array([P[j, i, :] for (i, j) in enumerate(d.iloc[-1].astype('int'))]),
@@ -207,8 +232,10 @@ print('Optimal Transition Matrix:\n')
 P_optim
 print('Optimal Reward Matrix:\n')
 R_optim
+
 # %% [markdown]
-# - **Calculate the value function for the Markov process with rewards that produces the highest expected discount rewards. (hint: use the transition probability and reward matrices from the previous question). Does the value function matches reasonably the value function from the previous question? (hint: it should)**
+#  - **Calculate the value function for the Markov process with rewards that produces the highest expected discount rewards. (hint: use the transition probability and reward matrices from the previous question). Does the value function matches reasonably the value function from the previous question? (hint: it should)**
+
 # %%
 q_sim = np.zeros(len(s) * len(e), dtype=np.float64)
 
@@ -226,8 +253,10 @@ for t in range(1, T + 1):
         v_sim[i, t] = rhs
 
 v_sim = pd.DataFrame(v_sim.T, columns=states)
+
 # %% [markdown]
-# - **Simulate the Markov process with rewards from the previous question for starting at each state pair $(e,s)$. Compute the average discounted reward. Does it match reasonabily close to the value function in the previous question? (hint: it should)**
+#  - **Simulate the Markov process with rewards from the previous question for starting at each state pair $(e,s)$. Compute the average discounted reward. Does it match reasonabily close to the value function in the previous question? (hint: it should)**
+
 # %%
 try:
     sim_data = joblib.load('sim_data.pkl')
@@ -269,7 +298,8 @@ table_comp.columns = ['Value Iteration', 'Value Optimal Policy', 'Simulated Aver
 table_comp
 
 # %% [markdown]
-# - **Calculate the optimal policy based on the policy iteration approach.**
+#  - **Calculate the optimal policy based on the policy iteration approach.**
+
 # %%
 q = np.zeros((len(a), len(s) * len(e)), dtype=np.float64)
 qq = np.zeros(len(s) * len(e), dtype=np.float64)
@@ -305,3 +335,22 @@ for t in range(1, T + 1):
 v = pd.DataFrame(v.T, columns=states)
 d = pd.DataFrame(d.T, columns=states)
 d.iloc[-1]
+
+
+# %%
+plot_data = np.zeros((len(s), len(e)))
+for i in range(len(states)):
+    row = eval(d.iloc[-1].index[i])[0]
+    column = eval(d.iloc[-1].index[i])[1]
+    plot_data[row, column] = d.iloc[-1, i]
+
+plt.plot(plot_data, drawstyle='steps')
+plt.xlabel('Scoops Available, s')
+plt.xticks(ticks=s)
+plt.ylabel('Optimal Policy - Scoops Saved')
+plt.yticks(a)
+plt.legend(e)
+plt.title('Scoops Available vs Scoops Saved')
+plt.show();
+
+
